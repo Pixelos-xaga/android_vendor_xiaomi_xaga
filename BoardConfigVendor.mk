ec195c52ed613782e1c5eab894d63f99a721e04b
Giovanni Ricca <giovanniricca@proton.me>
2024-04-15 09:58:04 +0200

    xaga: Extract from V816.0.8.0.ULOCNXM
    Change-Id: I5f47c17768dc0ef960054dfdb4ee2d9c9228bae0

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    apusys \
+    audio_dsp \
+    ccu \
+    dpm \
+    dtbo \
+    gpueb \
+    gz \
+    lk \
+    mcf_ota \
+    mcupm \
+    md1img \
+    mvpu_algo \
+    pi_img \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
