54265f86e1d5e20877c8c5374ec88f94dca8e96c
priiii08918 <priyanshusinghal0818@proton.me>
2025-03-16 16:16:44 +0000

    xaga: Drop firmware
    Change-Id: I2df8af440d648b0f77a07fbae53cd6e4de22d587

--- Android.mk
+++ Android.mk
@@ -3,26 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),xaga)
-
-$(call add-radio-file-sha1-checked,radio/apusys.img,300dcfd45697b8509a32d49874ad166c2878e92a)
-$(call add-radio-file-sha1-checked,radio/audio_dsp.img,03ca51c7803e94a64cc8fff8078c46518a170b41)
-$(call add-radio-file-sha1-checked,radio/ccu.img,f44ed8d2e82f434ee35250f7c4b0c9c584f33f84)
-$(call add-radio-file-sha1-checked,radio/dpm.img,2be6abe0556b280476a33e18fba296b480268ccd)
-$(call add-radio-file-sha1-checked,radio/dtbo.img,46005eec4ac9e7d75e1ddd14442ebdba9591bf22)
-$(call add-radio-file-sha1-checked,radio/gpueb.img,4542bf6785706fb1d9303f2235a0b7cde6760ac4)
-$(call add-radio-file-sha1-checked,radio/gz.img,1d473d901a944d8678c7ca643444b648dc8f2c62)
-$(call add-radio-file-sha1-checked,radio/lk.img,b3af3e996dc7e383fafff93401faef1a1bb846e9)
-$(call add-radio-file-sha1-checked,radio/mcf_ota.img,f13c977167b94c48e01832c8d5048e1ba04af554)
-$(call add-radio-file-sha1-checked,radio/mcupm.img,9e8960b306b3de557d9406b320aac31dedfd6847)
-$(call add-radio-file-sha1-checked,radio/md1img.img,20332d0efe2d40cf56502030b7a710978601b562)
-$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,7f48e0c88d04218f22237212d61d7cc3b77105af)
-$(call add-radio-file-sha1-checked,radio/pi_img.img,59c38550553e14dc082e2a3fdf87c14a5b045831)
-$(call add-radio-file-sha1-checked,radio/scp.img,2ef718f375c56aada35802ca44cfe8dc8f3c62a1)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,708bf91728d9fb83e0b1d1d1a44417eb8c6f49fc)
-$(call add-radio-file-sha1-checked,radio/sspm.img,3cccfa867282b02cfead3a8106c567c24e704a60)
-$(call add-radio-file-sha1-checked,radio/tee.img,3ded9ee397949f3a734c0e4ae36d18bd5bb58968)
-$(call add-radio-file-sha1-checked,radio/vcp.img,024598d46cd363561f26680274f57d0d227ee890)
-
-endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,23 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    apusys \
-    audio_dsp \
-    ccu \
-    dpm \
-    dtbo \
-    gpueb \
-    gz \
-    lk \
-    mcf_ota \
-    mcupm \
-    md1img \
-    mvpu_algo \
-    pi_img \
-    scp \
-    spmfw \
-    sspm \
-    tee \
-    vcp
