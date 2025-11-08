34d77dfc847def1ece567dd7bd363e3a6bd535f0
ChrisCatto <chriscatto@proton.me>
2025-11-08 15:56:36 +0000

    xaga: Sync with tree
    Change-Id: Iaed6d1278a41f97a0d463b7662a648417f4ab60a
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- xaga-vendor.mk
+++ xaga-vendor.mk
@@ -143,64 +143,9 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-camera.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-chg-only.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-chg-only.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-4k.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-4k.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-huanji.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-huanji.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-mgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-mgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-nolimits.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-nolimits.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-per-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-per-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-per-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-per-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-per-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-per-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-per-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-per-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-per-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-per-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-phone.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-phone.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-tgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-tgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-global-videochat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-global-videochat.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-hp-mgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-hp-mgame.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-hp-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-hp-normal.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-huanji.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-huanji.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-4k.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-4k.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-huanji.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-huanji.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-mgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-mgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-nolimits.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-nolimits.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-per-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-per-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-per-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-per-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-per-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-per-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-per-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-per-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-per-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-per-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-phone.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-phone.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-tgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-tgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-india-videochat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-india-videochat.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-4k.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-4k.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-huanji.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-huanji.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-mgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-mgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-nolimits.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-nolimits.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-camera.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-class0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-class0.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-huanji.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-huanji.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-navigation.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-normal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-per-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-per-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-phone.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-phone.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-tgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-tgame.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-video.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-l16u-videochat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-l16u-videochat.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-map-global.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-map-global.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-map-india.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-map-india.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-map-l16u-cn.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-map-l16u-cn.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-map.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-mgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-mgame.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-navigation.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-navigation.conf \
@@ -213,21 +158,9 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-per-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-per-normal.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-per-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-per-video.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-phone.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-phone.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-region-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-region-map.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-tgame.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-tgame.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-video.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermal-videochat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-videochat.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/disable_skin_control.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/disable_skin_control.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/disable_thermal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/disable_thermal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/disable_thermal_temp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/disable_thermal_temp.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/disable_throttling.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/disable_throttling.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_mtbf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_mtbf.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_policy_00.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_policy_00.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_policy_01.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_policy_01.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_policy_02.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_policy_02.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_policy_03.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_policy_03.conf \
-    vendor/xiaomi/xaga/proprietary/vendor/etc/thermal/thermal_policy_08.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal/thermal_policy_08.conf \
     vendor/xiaomi/xaga/proprietary/vendor/etc/thermald-devices.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald-devices.conf \
     vendor/xiaomi/xaga/proprietary/vendor/firmware/0_click_P_RTP.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/0_click_P_RTP.bin \
     vendor/xiaomi/xaga/proprietary/vendor/firmware/101_offlineCountdown_RTP.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/101_offlineCountdown_RTP.bin \
