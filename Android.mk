6abac4e038941c20940c0c350b098ba3cca3f325
ChrisCatto <chriscatto@proton.me>
2026-02-19 20:38:44 +0000

    xaga: Sync with tree
    Change-Id: I17beb71c48d2dc87e3ae256816f4e1cce6cca903
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,26 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),xaga)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,7ecf342b2a6058a1d1f98f49a1bc067bc667ce98)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,8989f363f2125d4f6b1d85020203084c9e937ce1)
+$(call add-radio-file-sha1-checked,radio/ccu.img,822ae01e2d3fc6e590b954ebfbd2867f4a9fcf8c)
+$(call add-radio-file-sha1-checked,radio/dpm.img,e9698184abfd3a5cfc7bd3c0475e85f3980164da)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,e09de919e3bdf0c90d537a532339c9fc9eb3a6ca)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,85478e1a446cb313af439c8857f71d060deef0a6)
+$(call add-radio-file-sha1-checked,radio/gz.img,87dd62fb86e2c1de9974c0c0877a3831b16a122d)
+$(call add-radio-file-sha1-checked,radio/lk.img,90022a22f306da671ad1c6700dda46204532b764)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,f3fd7ab13141d0f866d11a517646fb75f0565aed)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,0a21aa584e07b8eaff0cc1ed2958bbf81b5e2298)
+$(call add-radio-file-sha1-checked,radio/md1img.img,64c0737569d7d6930761616409530ca067ffc7d2)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,d27be59117e865d71f0db25c5b4e34fcde6c2b4c)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,9dccdb8b3df82fa0a5bd71badcb3da7264a913e0)
+$(call add-radio-file-sha1-checked,radio/scp.img,bf72c2539807347bb34fa21f3d54acd366f4683e)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,55b52579abd2c8129585a21326c07ddd29eb9d32)
+$(call add-radio-file-sha1-checked,radio/sspm.img,4cbd7fc3fadc010b8dafe7b4404970b64eb63a64)
+$(call add-radio-file-sha1-checked,radio/tee.img,919b8ca8e0dbaba26ed8ce2b9f7d3335765f3797)
+$(call add-radio-file-sha1-checked,radio/vcp.img,60e03b41adf5f44d0b4d378e945e2cc630eedabd)
+
+endif
